93ed36ddba549a6dd384d409a752ca558778a70a
linelect <pavlo.dudch@gmail.com>
2017-08-28 10:34:45 +0300

    delete dependency, change type

--- src/main/java/com/springcourse/Client.java
+++ src/main/java/com/springcourse/Client.java
@@ -1,12 +1,12 @@
 package com.springcourse;
 
-import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
 
     public static void main(String[] args) {
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext beanFactory = new ClassPathXmlApplicationContext("spring.xml");
         Event event = (Event)beanFactory.getBean("event");
         event.doEvent();
     }
